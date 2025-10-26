c08ab36d4e80add5529a655c99855530756f98ee
om kachhadiya <kachhadiyaom2005@gmail.com>
2025-10-26 18:35:53 +0530

    Start working on Routes and Controllers.

---
+++ server/controllers/post.js
@@ -0,0 +1,13 @@
+import PostMessage from "../models/postsMessage";
+
+export const getPosts = async(req,res) => {
+    try {
+        
+    } catch (error) {
+        
+    }
+}
+
+export const createPost = (req,res) => {
+    
+}
