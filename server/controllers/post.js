085e37895a93217a6fe3944e503639ac5574e066
om kachhadiya <kachhadiyaom2005@gmail.com>
2025-10-29 14:36:42 +0530

    Developing the Routes.

--- server/controllers/post.js
+++ server/controllers/post.js
@@ -2,12 +2,22 @@ import PostMessage from "../models/postsMessage";
 
 export const getPosts = async(req,res) => {
     try {
-        
+        const postsMessages  = await PostMessage.find();
+
+        res.status(200).json(postsMessages);
     } catch (error) {
-        
+        res.status(404).json({message: error.message});
     }
 }
 
-export const createPost = (req,res) => {
-    
+export const createPost = async(req,res) => {
+    const post = req.body;
+
+    const newPost = new postMessage(post);
+    try {
+        await newPost.save();
+        res.status(201).json(newPost);
+    } catch (error) {
+        res.status(409).json({message: error.message});
+    }
 }
