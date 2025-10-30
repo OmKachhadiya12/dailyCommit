2d486350b811d50272fbb6444e2a71209e085f3f
om kachhadiya <kachhadiyaom2005@gmail.com>
2025-10-30 12:21:20 +0530

    Starting the Fronted.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,19 @@
+import {Container,Button,Typography,Grow,Grid,AppBar} from "@mui/material";
+
+import memories from "./images/memories.png";
+
 function App() {
   
   return (
     <>
-      
+      <Container maxWidth="lg">
+          <AppBar position="static" color="inherit">
+            <Typography variant="h2" align="center">
+                Memories
+            </Typography>
+            <img src={memories} alt="Memories images" height="60"/>
+          </AppBar>
+      </Container>
     </>
   )
 }
