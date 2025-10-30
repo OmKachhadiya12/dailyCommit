71602c0c348e42801976700b5e4d5b76e780e568
om kachhadiya <kachhadiyaom2005@gmail.com>
2025-10-30 15:43:52 +0530

    Working on the Frontend.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,6 @@
 import {Container,Button,Typography,Grow,Grid,AppBar} from "@mui/material";
+import Posts from "./components/Posts/Posts.jsx";
+import Form from "./components/Form/Form.jsx";
 
 import memories from "./images/memories.png";
 
@@ -11,8 +13,20 @@ function App() {
             <Typography variant="h2" align="center">
                 Memories
             </Typography>
-            <img src={memories} alt="Memories images" height="60"/>
+            <img src={memories} alt="Memories images" height="500"/>
           </AppBar>
+          <Grow in>
+            <Container>
+              <Grid container justif="space-between" alignItems="stretch" spacing={4}>
+                <Grid item xs={12} sm={7}>
+                  <Posts/>
+                </Grid>
+                <Grid item xs={12} sm={4}>
+                  <Form/>
+                </Grid>
+              </Grid>
+            </Container>
+          </Grow>
       </Container>
     </>
   )
