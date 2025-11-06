74d4e4f717cd45a6106ccf6ee663b4d970de3d52
om kachhadiya <kachhadiyaom2005@gmail.com>
2025-11-06 14:24:10 +0530

    small bugs!

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -5,7 +5,13 @@ import App from './App.jsx'
 
 import {Provider} from "react-redux";
 import {createStore,applyMiddleware,compose } from "redux";
-import thunk from "react-thunk";
+import thunk from "redux-thunk";
+
+import reducers from "./reducers";
+
+const store = createStore(reducers,compose(applyMiddleware(thunk)));
+
+
 
 
 createRoot(document.getElementById('root')).render(
